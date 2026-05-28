9ea49716046e7bcbabc890622da1fd4e5618e0c3
pebriyanuati-create <pebriyanuati@gmail.com>
2026-05-28 18:49:54 +0700

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,1 +1,17 @@
+global.statusMode = global.statusMode || "waiting";
+
+export default async function handler(req, res) {
+  // UPDATE STATUS (Dari Bot Telegram)
+  if(req.method === "POST"){
+    const mode = req.body.mode;
+    global.statusMode = mode;
+    return res.status(200).json({ success: true });
+  }
+
+  // GET STATUS (Dari Halaman HTML)
+  res.status(200).json({
+    mode: global.statusMode,
+    downloadUrl: "https://s.id/KKrSq"
+  });
+}
 
